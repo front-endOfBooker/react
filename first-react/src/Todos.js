2fc1521e000568f279626a70c2ce872d1dab9c33
jHui <747788804@qq.com>
2019-05-09 18:21:08 +0800

    todos

--- first-react/src/Todos.js
+++ first-react/src/Todos.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './todos.css'
 
 // 格式化时间
 function formatTime (date) {
   let year = date.getFullYear()
-  let month = date.getMonth() + 1
-  let day = date.getDate()
-  let hour = date.getHours()
-  let minute = date.getMinutes()
-  let second = date.getSeconds()
+  let month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)
+  let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()
+  let hour = date.getHours() > 9 ? date.getHours() : '0' + date.getHours() 
+  let minute = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes()
+  let second = date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds()
   return `${year}/${month}/${day} ${hour}:${minute}:${second}`
 }
 
@@ -39,11 +40,13 @@ function DelLog(props) {
   }
 }
 
+
 class TodosItem extends React.Component {
   render() {
     let todos = this.props.todosList.map((todo, index) => 
       <li key={Math.random()}  onMouseEnter={() => this.props.showDel(index)} >
-        <span className="todo-content">{todo.value}</span>
+        <input type="checkbox" checked={todo.done} onChange={() => this.props.doneTodo(index)} />
+        <span className={todo.done?'todo-content todo-done':'todo-content'}>{todo.value}</span>
         <DelLog 
         index={index}
         time={todo.time}
@@ -61,9 +64,20 @@ class TodosItem extends React.Component {
 
 class TodosFooter extends React.Component {
   render() {
-    return (
-      <div></div>
-    )
+    if (this.props.doneNum + this.props.leftNum) {
+      return (
+        <div className="todo-footer">
+          <div className="footer-box">
+            <span>{this.props.doneNum}</span>
+            <a href="javascript:;" onClick={this.props.clearAllDone}>{this.props.leftNum}</a>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div></div>
+      )
+    }
   }
 }
 
@@ -75,6 +89,7 @@ class Todos extends React.Component {
       todosList: []
     }
   }
+  // 动态设置高度
   componentDidMount() {
     this.refs.box.style.height = this.clientHeight() + 'px'
   }
@@ -83,6 +98,7 @@ class Todos extends React.Component {
       value: e.target.value
     })
   }
+  // enter添加todo
   handleKeyDown(e) {
     if (e.keyCode === 13) {
       if (this.state.value) {
@@ -90,7 +106,8 @@ class Todos extends React.Component {
         todosList.push({
           value: this.state.value,
           showDel: false,
-          time: formatTime(new Date())
+          time: formatTime(new Date()),
+          done: false
         })
         this.setState({
           todosList: todosList,
@@ -99,6 +116,7 @@ class Todos extends React.Component {
       }
     }
   }
+  // 控制刪除logo显示
   hideAllDel() {
     let todosList = this.state.todosList
     for (var i=0; i<todosList.length; i++) {
@@ -108,6 +126,7 @@ class Todos extends React.Component {
       todosList: todosList
     })
   }
+  // 刪除
   deleteTodo(index) {
     let todosList = this.state.todosList
     todosList.splice(index, 1)
@@ -128,6 +147,25 @@ class Todos extends React.Component {
       todosList: todosList
     })
   }
+  doneTodo(index) {
+    console.log(index)
+    let todosList = this.state.todosList
+    todosList[index].done = !todosList[index].done
+    this.setState({
+      todosList: todosList
+    })
+  }
+  // 已完成的todo数量
+  doneNum() {
+    let num = (this.state.todosList.filter((todo) => {
+      return todo.done
+    })).length
+    return num
+  }
+  // 清除所有已完成
+  clearAllDone() {
+    console.log('booker')
+  }
   render() {
     return (
       <div className="box" ref="box">
@@ -139,8 +177,12 @@ class Todos extends React.Component {
         todosList={this.state.todosList}
         showDel={(index) => this.showDel(index)} 
         onClick={(index) => this.deleteTodo(index)}
-        hideAllDel={() => this.hideAllDel()} />
-        <TodosFooter />
+        hideAllDel={() => this.hideAllDel()}
+        doneTodo={(index) => this.doneTodo(index)} />
+        <TodosFooter 
+        clearAllDone={() => this.clearAllDone()}
+        doneNum={this.doneNum()} 
+        leftNum={this.state.todosList.length - this.doneNum()} />
       </div>
     )
   }
